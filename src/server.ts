47fbda4e005393e42c2879a2a7d21ef86b68ab65
wallacemachado <wallacefm18@gmail.com>
2021-05-10 18:33:21 -0300

    Debugando a aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 
 const app = express();
+
+app.use(express.json());
  
-app.get("/", (reuqest, response)=> {
-  return response.json({message: "Hello world!"})
-})
+app.get("/", (request, response)=> {
+  return response.json({message: "Hello world!"});
+});
+
+app.post("/courses", (request, response)=> {
+  const { name } = request.body;
+  return response.json({name});
+});
 
 app.listen(3333, () => console.log('Server is runnig!'));
