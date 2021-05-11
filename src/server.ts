23bf068d5e907b290769671eb01fa1ac21618ad6
wallacemachado <wallacefm18@gmail.com>
2021-05-11 14:16:31 -0300

    Criando categoria in memoria

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './categories.routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(categoriesRoutes);
  
-app.get("/", (request, response)=> {
-  return response.json({message: "Hello world!"});
-});
-
-app.post("/courses", (request, response)=> {
-  const { name } = request.body;
-  return response.json({name});
-});
-
 app.listen(3333, () => console.log('Server is runnig!'));
