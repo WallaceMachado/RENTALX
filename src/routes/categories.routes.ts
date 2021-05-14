d265205afafc4909bc7c68b27d332a17667005c3
wallacemachado <wallacefm18@gmail.com>
2021-05-13 21:19:40 -0300

    criando upload de arquivo com insomnia

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/useCases/CreateCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategory';
@@ -7,6 +7,9 @@ import { listCategoryController } from '../modules/cars/useCases/listCategory';
 
 const categoriesRoutes = Router();
 
+const upload = multer({
+  dest: "./tmp",
+});
 
 
 //path inicial da rota está no server
@@ -16,6 +19,13 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoryController.hangle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, respose) => {
+  const { file } = request;
+
+  console.log(file);
+  return respose.send();
 });
 
 export { categoriesRoutes };
