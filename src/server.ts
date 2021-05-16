57d5fa7c65dd60f05c05685c2d118354c00650f7
wallacemachado <wallacefm18@gmail.com>
2021-05-16 17:35:17 -0300

    configiração inicial swagger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import swaggerUI from 'swagger-ui-express';
-import swaggerFile from './swagger.json'
 import { router } from './routes';
-
+import swaggerFile from './swagger.json';
 
 const app = express();
 
