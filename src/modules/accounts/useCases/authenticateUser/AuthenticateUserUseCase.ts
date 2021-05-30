b0fd73252120c3b0156b08f961e6788585721dc6
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-05-30 19:12:40 -0300

    criando token do usuário

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,68 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  },
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) { }
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    //Verificar se usuário existe
+
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect!");
+    }
+
+
+    //verificar se senha está correta
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect!");
+    }
+
+    // Gerar jsonwebtoken , gerar md5 aleatótio, expiresIn - tempo para experar o token (1dia) 
+
+    const token = sign({}, "c9b3d086beed799c29c857496acf4646", {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    const tokenReturn: IResponse ={
+      token,
+      user:{
+        name: user.name,
+        email: user.email,
+      }
+    };
+
+    return tokenReturn;
+
+  }
+
+}
+
+export { AuthenticateUserUseCase }
