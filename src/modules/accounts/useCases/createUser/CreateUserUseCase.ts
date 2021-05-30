964dc5485a8ea4171498fd671047c77afe551c68
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-05-30 17:01:12 -0300

    add criptografia de senha com bcryptjs

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcryptjs";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -11,10 +12,13 @@ class CreateUserUseCase {
   ) { }
 
   async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
