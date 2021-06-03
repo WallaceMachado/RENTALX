bc54e36505f00774a7504964957cf31e5ae1caf5
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-06-03 20:48:24 -0300

    teste de criação de categoria com jest

--- src/modules/cars/useCases/CreateCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/CreateCategory/CreateCategoryUseCase.spec.ts
@@ -1,16 +1,50 @@
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory';
+import { AppError } from '../../../../errors/AppError';
 
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
-describe("Criar categoria", () =>{
-  it("Espero que 2 + 2 seja 4", ()  => {
-    const soma = 2 + 2;
-    const resultado = 4;
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
 
-    expect(soma).toBe(resultado);
-  });
-  it("Espero que 2 + 2 não seja 5", ()  => {
-    const soma = 2 + 2;
-    const resultado = 5;
+describe('Create Category', () => {
 
-    expect(soma).not.toBe(resultado);
-  });
-});
+    //essa function é chama antes do teste
+    beforeEach(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+        createCategoryUseCase = new CreateCategoryUseCase(
+            categoriesRepositoryInMemory
+        );
+    });
+
+    it('Should be able to create new category', async () => {
+        const category = {
+            name: 'SUV',
+            description: 'Carros grandes',
+        };
+
+        await createCategoryUseCase.excute({name:'SUV', description: 'Carros grandes', })
+
+        //como é uma function void, vamos usar o método findByName para verificar se a categoria foi criada
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(
+            category.name
+        );
+          console.log(categoryCreated);
+        // espera que categoryCreated tenha a propriedade "id".
+        expect(categoryCreated).toHaveProperty('id');
+    });
+
+    it('Should not be able to create category with an existing name', async () => {
+        expect(async () => {
+            const category = {
+                name: 'SUV',
+                description: 'Carros grandes',
+            };
+
+            await createCategoryUseCase.excute({name:category.name, description: category.description});
+
+            
+            await createCategoryUseCase.excute({name:category.name, description: category.description});
+        }).rejects.toBeInstanceOf(AppError);
+    }); 
+}); 
+
