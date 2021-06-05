ddebead3445bd5857eed6b1847391db7fbd0c6e8
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-06-04 22:02:32 -0300

    criando migration de carro

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -1,8 +1,9 @@
 import { getRepository, Repository } from "typeorm";
 import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
-
 import { User } from "../entities/User";
+
+
 
 
 
