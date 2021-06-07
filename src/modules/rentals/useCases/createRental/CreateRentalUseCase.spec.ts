b9a9c66a177d684fe38992efc95f0a124a18bd03
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-06-07 20:56:09 -0300

    add cadastro e aluguel de aluguel de carros

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,83 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
+
+
+let rentalsRepository: RentalsRepositoryInMemory;
+//let dayjsDateProvider: DayjsDateProvider;
+let createRentalUseCase: CreateRentalUseCase;
+let carsRepository: CarsRepositoryInMemory;
+
+let sampleRental_Car1_User1: ICreateRentalDTO;
+let sampleRental_Car2_User1: ICreateRentalDTO;
+let sampleRental_Car1_User2: ICreateRentalDTO;
+let sampleRentalLessThan24h: ICreateRentalDTO;
+
+describe('Create Rental', () => {
+  beforeEach(() => {
+      rentalsRepository = new RentalsRepositoryInMemory();
+    //  dayjsDateProvider = new DayjsDateProvider();
+      createRentalUseCase = new CreateRentalUseCase(
+          rentalsRepository,
+          // dayjsDateProvider,
+        //  carsRepository,
+         
+          
+      );
+  });
+
+  beforeAll(() => {
+     // const new Date() = dayjs().add(1, 'day').toDate();
+      sampleRental_Car1_User1 = {
+          car_id: '12345',
+          user_id: '54321',
+          expected_return_date: new Date(),
+      };
+      sampleRental_Car2_User1 = {
+          car_id: '23456',
+          user_id: '54321',
+          expected_return_date: new Date(),
+      };
+      sampleRental_Car1_User2 = {
+          car_id: '12345',
+          user_id: '65432',
+          expected_return_date: new Date(),
+      };
+    /*  sampleRentalLessThan24h = {
+          car_id: '12345',
+          user_id: '54321',
+          expected_return_date: dayjs().toDate(),
+      };*/
+  });
+
+  it('Should be able to create a new rental', async () => {
+      const rental = await createRentalUseCase.execute(
+          sampleRental_Car1_User1
+      );
+
+      expect(rental).toHaveProperty('id');
+      expect(rental).toHaveProperty('start_date');
+  });
+
+  it('Should not be able to create a new rental if user already has an ongoing one', () => {
+      expect(async () => {
+          await createRentalUseCase.execute(sampleRental_Car1_User1);
+          await createRentalUseCase.execute(sampleRental_Car2_User1);
+      }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to create a new rental if car has already been rented', () => {
+      expect(async () => {
+          await createRentalUseCase.execute(sampleRental_Car1_User1);
+          await createRentalUseCase.execute(sampleRental_Car1_User2);
+      }).rejects.toBeInstanceOf(AppError);
+  });
+/*
+  it('Should not be able to create a new rental if rental duration is less than 24 hours', () => {
+      expect(async () => {
+          await createRentalUseCase.execute(sampleRentalLessThan24h);
+      }).rejects.toBeInstanceOf(AppError);
+  });*/
+});
---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -0,0 +1,73 @@
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { AppError } from "@shared/errors/AppError";
+
+
+
+interface IRequest {
+  user_id: string;
+  car_id: string;
+  expected_return_date: Date;
+}
+
+class CreateRentalUseCase {
+
+  constructor(
+   // @inject('RentalsRepository')
+    private rentalsRepository: IRentalsRepository,
+
+   // @inject('DayjsDateProvider')
+//private dateProvider: IDateProvider,
+
+   // @inject('CarsRepository')
+  //  private carsRepository: ICarsRepository
+) {}
+
+async execute({
+    car_id,
+    user_id,
+    expected_return_date,
+}: IRequest): Promise<Rental> {
+    const carUnavailable =
+        await this.rentalsRepository.findOngoingRentalByCar(car_id);
+
+    if (carUnavailable) {
+        throw new AppError('Car currently rented!');
+    }
+
+    const userUnavailable =
+        await this.rentalsRepository.findOngoingRentalByUser(user_id);
+
+    if (userUnavailable) {
+        throw new AppError('User can only rent one car at a time!');
+    }
+
+    /*
+    const duration = this.dateProvider.compareInHours(
+        this.dateProvider.dateNow(),
+        expected_return_date
+    );
+
+    const minimumRentalDuration = 24;
+
+    if (duration < minimumRentalDuration) {
+        throw new AppError(
+            'Rental duration needs to be longer than 24 hours!'
+        );
+    } */
+
+    const rental = await this.rentalsRepository.create({
+        car_id,
+        user_id,
+        expected_return_date,
+    });
+
+  // await this.carsRepository.updateAvailability(car_id, false);
+
+    return rental;
+}
+
+}
+
+export { CreateRentalUseCase }
