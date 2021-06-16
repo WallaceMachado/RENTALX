16e2dd12ceef94acb80cc4bf6c572d951784ad83
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-06-16 12:02:11 -0300

    configurando storage S3 aws-sdk parte 3 - Upload com s3

--- src/shared/container/provaiders/index.ts
+++ src/shared/container/provaiders/index.ts
@@ -4,6 +4,7 @@ import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvi
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 
@@ -18,7 +19,13 @@ container.registerInstance<IMailProvider>(
   new EtherealMailProvider()
 )
 
-container.registerSingleton<IStorageProvider>(
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider
+ }
+ 
+ container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  LocalStorageProvider
-)
+  diskStorage[process.env.disk]
+ 
+ )
