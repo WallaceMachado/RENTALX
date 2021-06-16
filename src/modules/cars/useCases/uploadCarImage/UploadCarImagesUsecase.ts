d48b8f2b30b79cc6d9a7a9163c8f29da643d3553
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-06-16 17:20:24 -0300

    criando url de acesso do avatar(foto) com class-transformer

--- src/modules/cars/useCases/uploadCarImage/UploadCarImagesUsecase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImagesUsecase.ts
@@ -1,4 +1,5 @@
 import { ICarImagesRepository } from "@modules/cars/repositories/ICarImagesRepository";
+import { IStorageProvider } from "@shared/container/provaiders/StorageProvider/IStorageProvider";
 import { inject, injectable } from "tsyringe";
 
 
@@ -7,12 +8,15 @@ class UploadCarImagesUseCase {
 
   constructor(
     @inject('CarImagesRepository')
-    private carImagesRepository: ICarImagesRepository
+    private carImagesRepository: ICarImagesRepository,
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) { }
 
   async execute(car_id: string, images_name: string[]): Promise<void> {
     images_name.map(async (image) => {
       await this.carImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -12,6 +12,7 @@ import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
 import swaggerFile from '../../../swagger.json';
 import { router } from "./routes";
+import upload from "@config/upload";
 
 createConnection();
 const app = express();
@@ -19,6 +20,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(router);
 
