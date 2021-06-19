54fbdb67966330c2a50c25ad0af675f85b41ac5f
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-06-19 16:23:20 -0300

    add sentry para log de erros

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import "express-async-errors"
 import swaggerUI from 'swagger-ui-express';
 import cors from "cors";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 import createConnection from "@shared/infra/typeorm";
 
@@ -21,7 +23,21 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    
+    new Sentry.Integrations.Http({ tracing: true }),
+   
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 app.use(express.json());
+
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
@@ -30,6 +46,10 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
+
+app.use(
+  Sentry.Handlers.errorHandler()
+);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
