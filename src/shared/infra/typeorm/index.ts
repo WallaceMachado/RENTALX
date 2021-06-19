1985a3dbd3b5da09de22e2f4c145e308faaa8b8a
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2021-06-19 08:33:57 -0300

    ajustes para rodar aplicação fora do docker, via yarn dev

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,17 +1,16 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 
-export default async (host = 'database'): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
-      Object.assign(defaultOptions, {
-          host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
-          database:
-              process.env.NODE_ENV === 'test'
-                  ? 'rents_test'
-                  : defaultOptions.database,
-      })
-      
+    Object.assign(defaultOptions, {
+      database:
+        process.env.NODE_ENV === 'test'
+          ? 'rents_test'
+          : defaultOptions.database,
+    })
+
   );
 };
